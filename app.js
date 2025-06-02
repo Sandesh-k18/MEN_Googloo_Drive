91c364b08f20a680cb75bf5f90c07d09accbcf6c
Sandesh <pokhrelamrita03@gmail.com>
2025-06-02 21:18:10 +0545

    Credentials Manager

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+app.set("viewengine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Googloo Drive With Authenticatioin");
 });
+
+app.listen(3000);
