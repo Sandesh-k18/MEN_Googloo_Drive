4b08f6e6c46bf0686de77de79b04bca1ecd53b45
Sandesh-k18 <sandeshkharel18@gmail.com>
2025-06-03 10:21:06 +0545

    user route test

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/test", (req, res) => [res.send("User Route1")]);
+router.get("/test", (req, res) => {
+  res.send("User Route1");
+});
 
 module.exports = router;
