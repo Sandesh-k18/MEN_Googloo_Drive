d7703e4ad9d6b6f231a37c1ba9fc768d73ab54dc
Sandesh-k18 <sandeshkharel18@gmail.com>
2025-06-04 19:25:16 +0545

    Login Authentication with cookies SET⚡

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const userRouter = require("./routes/user.routes");
 const router = require("./routes/user.routes");
+const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 const connectToDB = require("./config/db");
@@ -10,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use("/user", userRouter);
-
+app.use(cookieParser());
 app.listen(3000, () => {
   console.log("Server runnig at port 3000");
 });
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const { body, validationResult } = require("express-validator");
 const router = express.Router();
 const userModel = require("../models/user.models");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.get("/test", (req, res) => {
   res.send("User Route1");
@@ -34,6 +35,50 @@ router.post(
       password: hashPassword,
     });
     res.json(newUser);
+  }
+);
+router.get("/login", (rew, res) => {
+  res.render("login");
+});
+router.post(
+  "/login",
+  body("username").trim().isLength({ min: 4 }),
+  body("password").trim().isLength({ min: 8 }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        error: errors.array(),
+        message: "Invalid Data",
+      });
+    }
+    const { username, password } = req.body;
+    const user = await userModel.findOne({
+      username: username,
+    });
+    if (!user) {
+      return res.status(400).json({
+        message: "Username or Password is wrong",
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Username or Password is wrong",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        email: user.email,
+        username: user.username,
+      },
+      process.env.JWT_SECRET
+    );
+    console.log(req.body);
+    res.cookie("token", token);
+    res.send("Logged in");
   }
 );
 
