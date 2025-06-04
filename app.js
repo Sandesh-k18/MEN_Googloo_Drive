fee8e4ed3f1847b151e726e5eeb27570946bc63e
Sandesh-k18 <sandeshkharel18@gmail.com>
2025-06-04 22:05:53 +0545

    Home page UI

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const userRouter = require("./routes/user.routes");
 const router = require("./routes/user.routes");
+const indexRouter = require("./routes/index.routes");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -10,6 +11,7 @@ connectToDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use("/", indexRouter);
 app.use("/user", userRouter);
 app.use(cookieParser());
 app.listen(3000, () => {
